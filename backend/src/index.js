5bc89f6d2a90d0a1d32b67f33038555c29b05a53
mksaiyed <mrmksaiyed@gmail.com>
2024-10-09 22:51:40 +0530

    Test case | 2. It should return the numebr itself if it's valid number given as a string

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,13 @@ function add(numberString) {
     if (!numberString?.length) {
         return 0;
     }
+
+    // check for the single input
+    if (numberString?.length === 1) {
+        // return 0 if it's Not A Number
+        if (isNaN(parseInt(numberString))) return 0;
+        return parseInt(numberString);
+    }
 }
 
 module.exports = { add };
--- backend/src/tests/stringCalculator.test.js
+++ backend/src/tests/stringCalculator.test.js
@@ -4,4 +4,10 @@ describe("Test cases for string calculator", () => {
     test("1. For empty string it should return 0", () => {
         expect(add("")).toBe(0);
     });
+
+    test("2. It should return the numebr itself if it's valid number given as a string", () => {
+        expect(add("1")).toBe(1);
+        expect(add("5")).toBe(5);
+        expect(add("k")).toBe(0);
+    });
 });
