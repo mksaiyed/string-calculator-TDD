72d86686c69b302d4e1faf39b7ee86def9594013
mksaiyed <mrmksaiyed@gmail.com>
2024-10-16 18:08:43 +0530

    feat: Added state managemnet for the input box and the event handler for add button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [text, setText] = useState("");
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    };
+
+    const handleAddButtonClick = () => {
+        // handle add button click functionality
+    };
     return (
         <>
             <h1>String Calculator</h1>
@@ -9,9 +18,11 @@ function App() {
                 <input
                     className="inputText"
                     type="text"
+                    value={text}
+                    onChange={handleChange}
                     placeholder="Enter numner with comma seperater"
                 />
-                <button>Add</button>
+                <button onClick={handleAddButtonClick}>Add</button>
             </div>
         </>
     );
