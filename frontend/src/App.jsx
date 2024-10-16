f9acfdbd80c9fa0dbd47e4fcc11f7907be2256f7
mksaiyed <mrmksaiyed@gmail.com>
2024-10-16 18:06:47 +0530

    feat: Input box and add button UI added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,14 @@ function App() {
     return (
         <>
             <h1>String Calculator</h1>
+            <div>
+                <input
+                    className="inputText"
+                    type="text"
+                    placeholder="Enter numner with comma seperater"
+                />
+                <button>Add</button>
+            </div>
         </>
     );
 }
